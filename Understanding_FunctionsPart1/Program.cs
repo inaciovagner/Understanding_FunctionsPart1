948d4b69d64951c44bde5268a0f89277652d4e97
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-22 16:41:44 +0100

    Add project files.

---
+++ Understanding_FunctionsPart1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CsharpPrograms
+{
+    class Program
+    {
+        static public int Average(float a, float b, float c, float d, float e)
+        {
+            int avg = (int)((a + b + c + d + e) / 5);
+            return avg;
+        }
+        static void Main(string[] args)
+        {
+            float v, w, x, y, z;
+            int result = 0;
+            v = float.Parse(Console.ReadLine());
+            w = float.Parse(Console.ReadLine());
+            x = float.Parse(Console.ReadLine());
+            y = float.Parse(Console.ReadLine());
+            z = float.Parse(Console.ReadLine());
+            result = Average(v, w, x, y, z);
+            Console.WriteLine(result);
+        }
+    }
+}
